4ea7331647fa99adc19e401274575971dcb02e6b
yuanchuan <yuanchuan23@gmail.com>
2024-10-06 00:00:14 +0800

    Fix flags on Linux

--- src/static.js
+++ src/static.js
@@ -1,10 +1,11 @@
 import fs from 'node:fs';
 import { dirname, join } from 'node:path';
 import { fileURLToPath } from 'node:url';
-import { homedir } from 'node:os';
+import os from 'node:os';
 
+const isLinux = os.platform() === 'linux';
 const root = dirname(fileURLToPath(import.meta.url));
-const configDir = join(homedir(), '.css-doodle');
+const configDir = join(os.homedir(), '.css-doodle');
 const configFilePath = join(configDir, 'config.json');
 
 if (!fs.existsSync(configDir)) {
@@ -19,6 +20,26 @@ function read(path) {
     return fs.readFileSync(join(root, path), 'utf8');
 }
 
+function getDefaultAppArgs() {
+    const args = [
+        '--disable-infobars',
+        '--enable-experimental-web-platform-features',
+        '--diable-extensions',
+        '--enable-gpu-rasterization',
+        '--enable-oop-rasterization',
+        '--enable-webgl-draft-extensions',
+        '--enable-composited-animations',
+        '--enable-optimized-css',
+        '--enable-accelerated-2d-canvas',
+    ];
+
+    if (isLinux) {
+        args.push('--no-sandbox', '--disable-setuid-sandbox');
+    }
+
+    return args;
+}
+
 export const config = JSON.parse(fs.readFileSync(configFilePath), 'utf8');
 export const configPath = configFilePath;
 
@@ -26,17 +47,4 @@ export const pkg = JSON.parse(read('../package.json'));
 export const cssDoodleLib = read('../node_modules/css-doodle/css-doodle.min.js');
 export const previewClient = read('./preview/client.html');
 export const previewServerPath = join(root, './preview/server.js');
-
-export const defaultAppArgs = [
-    '--disable-infobars',
-    '--enable-experimental-web-platform-features',
-    '--diable-extensions',
-    '--enable-gpu-rasterization',
-    '--enable-oop-rasterization',
-    '--enable-webgl-draft-extensions',
-    '--enable-composited-animations',
-    '--enable-optimized-css',
-    '--enable-accelerated-2d-canvas',
-    '--no-sandbox',
-    '--disable-setuid-sandbox',
-];
+export const defaultAppArgs = getDefaultAppArgs();
